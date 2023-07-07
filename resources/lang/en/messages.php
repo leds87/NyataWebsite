e31d899e3cb32446b051cb1f6ff00b885b9f0f36
Randy Kurnia <clodeo.randy@sicepat.com>
2023-07-07 11:42:43 +0700

    add content lang

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,16 +3,16 @@
 // lang/en/messages.php
 
 return [
-    'Welcome' => 'Welcome to our application!',
-    'Test' => 'Test is successful',
-    'Join Nyata Foundation and help to change the world to become a better place' => 'Join Nyata Foundation and help to change the world to become a better place',
-    'Welcome to our website' => 'Welcome to our website',
-    'THE ISSUE' => 'THE ISSUE',
-    'TheIssueDescription' => 'The Ministry of Education and Culture noted that there were 157,000 elementary to high school
-    students dropping out of school in the 2019/2020 school year. Most students who drop out of school
-    are at the elementary school level as many as 59,400 students. This is where NYATA moves to help the
-    goverment and Indonesian children to reduce the high dropout rate and hopes that Indonesian children
-    will continue to get a proper education through several programs that NYATA has',
+    // 'Welcome' => 'Welcome to our application!',
+    // 'Test' => 'Test is successful',
+    // 'Join Nyata Foundation and help to change the world to become a better place' => 'Join Nyata Foundation and help to change the world to become a better place',
+    // 'Welcome to our website' => 'Welcome to our website',
+    // 'THE ISSUE' => 'THE ISSUE',
+    // 'TheIssueDescription' => 'The Ministry of Education and Culture noted that there were 157,000 elementary to high school
+    // students dropping out of school in the 2019/2020 school year. Most students who drop out of school
+    // are at the elementary school level as many as 59,400 students. This is where NYATA moves to help the
+    // goverment and Indonesian children to reduce the high dropout rate and hopes that Indonesian children
+    // will continue to get a proper education through several programs that NYATA has',
 
     //share page
     'JoinNyata' => 'Join Nyata',
@@ -23,6 +23,7 @@ return [
     'GiveSupport' => 'Give Support',
     'SendYourDonation' => 'Send Your Donation To',
     'Join' => 'Join',
+    "LearnMore" => "Learn More",
 
     //menu
     'DonateMenu' => 'Donate',
@@ -34,26 +35,45 @@ return [
     'ActOfKindnessMenu' => 'Act Of Kindness',
     'TakeActionMenu' => 'Take Action',
 
-    //index page
-    'WHO WE ARE' => 'WHO WE ARE',
-    'WhoWeAreNote' => 'NYATA Foundation is a non profit organization works on improving the children with low quality of life. Start with the name “Tanah Air Foundation” rose in 2014 and to adjust thename now becomes it is known as Yayasan Anak-anak Tanah Air (NYATA)',
-    'WHAT WE HOLD' => 'WHAT WE HOLD',
-    'SHARING' => 'SHARING',
-    'SharingNote' => 'Help, share knowledge and joy is the way of NYATA',
-    'EmpowermentNote' => 'Nyata improves the value of individuals, social and education quality that focus on children quality of life and their education',
-    'ProfessionalNote' => 'Nyata focus on education and skill by giving the high quality training to volunteers and teachers',
-    'EducationIsTheHeartNote' => 'Help the children to get proper education. Every help will keep the children to go to schools and make a better future.',
-    'OUR SOLUTION' => 'OUR SOLUTION',
-    'AdikAsuhNote' => 'Join us to be foster brother and sister and help the children reach a brigther future',
-    'TravelCharityNote' => 'Help local livelihoods and economic communities while travel to explore the beauty of Indonesia',
-    'ActOfKindnessNote' => 'Gives the good deeds to the school, organization, individual or community that in need',
+    // //index page
+    // 'WHO WE ARE' => 'WHO WE ARE',
+    // 'WhoWeAreNote' => 'NYATA Foundation is a non profit organization works on improving the children with low quality of life. Start with the name “Tanah Air Foundation” rose in 2014 and to adjust thename now becomes it is known as Yayasan Anak-anak Tanah Air (NYATA)',
+    // 'WHAT WE HOLD' => 'WHAT WE HOLD',
+    // 'SHARING' => 'SHARING',
+    // 'SharingNote' => 'Help, share knowledge and joy is the way of NYATA',
+    // 'EmpowermentNote' => 'Nyata improves the value of individuals, social and education quality that focus on children quality of life and their education',
+    // 'ProfessionalNote' => 'Nyata focus on education and skill by giving the high quality training to volunteers and teachers',
+    // 'EducationIsTheHeartNote' => 'Help the children to get proper education. Every help will keep the children to go to schools and make a better future.',
+    // 'OUR SOLUTION' => 'OUR SOLUTION',
+    // 'AdikAsuhNote' => 'Join us to be foster brother and sister and help the children reach a brigther future',
+    // 'TravelCharityNote' => 'Help local livelihoods and economic communities while travel to explore the beauty of Indonesia',
+    // 'ActOfKindnessNote' => 'Gives the good deeds to the school, organization, individual or community that in need',
 
 
-    //Donate page
-    'YouCanMakeNote' => 'TEMAN NYATA is a monthly giving program where every dollar goes toward educating a child. You are making an impact in life of someone else, every single month.',
-    'AffordableNote' => 'By giving monthly, you invest what makes sense for you and make an incredible impact over the course of your membership',
-    'SustainedSupportNote' => 'Enables NYATA to expand our programs because we know we have the ongoing support from the NYATA Partner community',
-    'CommunityNote' => 'Becoming a NYATA Partner allows you to join a dedicated group of supporters who understand the importance of making a sustaining commitment to provide education for all.',
-    'ItsExcitingNote' => 'NYATA Partner will receive special updates, photos and videos of the children and communities you’ve impacted and more.',
-    'YouWillReceieveNote' => 'Quarterly progress updates from the field, Welcome gift, Surprise end of the year sourvenir, photos and videos from the field, and more!'
+    // //Donate page
+    // 'YouCanMakeNote' => 'TEMAN NYATA is a monthly giving program where every dollar goes toward educating a child. You are making an impact in life of someone else, every single month.',
+    // 'AffordableNote' => 'By giving monthly, you invest what makes sense for you and make an incredible impact over the course of your membership',
+    // 'SustainedSupportNote' => 'Enables NYATA to expand our programs because we know we have the ongoing support from the NYATA Partner community',
+    // 'CommunityNote' => 'Becoming a NYATA Partner allows you to join a dedicated group of supporters who understand the importance of making a sustaining commitment to provide education for all.',
+    // 'ItsExcitingNote' => 'NYATA Partner will receive special updates, photos and videos of the children and communities you’ve impacted and more.',
+    // 'YouWillReceieveNote' => 'Quarterly progress updates from the field, Welcome gift, Surprise end of the year sourvenir, photos and videos from the field, and more!',
+
+    //ABOUT US PAGE
+    "FoundingHistory" => "Founding History",
+    "FoundingHistoryDescription" => "Started from the social service program held in high school every year, Nicole Oeij Teng, Pamela Berliana Sofian, and their friends and relatives were moved to continue their social service. Until 2013, Nicole decided to volunteer to teach in underprivileged schools and communites in Africa for 2 months. This experience gave birth an idea to help underprivileged and out of school children in Indonesia. This non-profit foundation was officially founded in 2014 by Nicole and Pamela and it keeps growing since then.",
+    "OurJourney" => "Our Journey",
+    "Home" => "Home",
+    "HomeDescription" => "Mountain, a historic symbol of Indonesia, represents home to the children we are taking care of.",
+    "Education" => "Education",
+    "EducationDescription" => "Education as the base of our value that we hold tightly to.",
+    "Children" => "Children",
+    "ChildrenDescription" => "Like a flower that is blooming, we focus on nurturing children of the nation.",
+    "Light" => "Light",
+    "LightDescription" => "Our children of light are our enlightment to the future",
+    "OurResults" => "Our Results",
+    "TagLine" => "Anak Nyata, Anak Terang",
+    "TagLineDescription" => "Anak terang dari negeri Indonesia.",
+
+    //TITLE
+    "AboutUsTitle" => "ABOUT US",
 ];
--- resources/lang/ru/messages.php
+++ resources/lang/ru/messages.php
@@ -4,10 +4,10 @@
 
 return [
 
-    'Welcome' => 'Selamat datang di aplikasi Nyata',
-    'Test' => 'Добро пожаловать ',
-    'Join Nyata Foundation and help to change the world to become a better place' => 'Bergabung dengan Nyata Foundation dan bantu merubah dunia menjadi lebih baik',
-    'Welcome to our website' => 'Добро пожаловать на наш сайт',
+    // 'Welcome' => 'Selamat datang di aplikasi Nyata',
+    // 'Test' => 'Добро пожаловать ',
+    // 'Join Nyata Foundation and help to change the world to become a better place' => 'Bergabung dengan Nyata Foundation dan bantu merubah dunia menjadi lebih baik',
+    // 'Welcome to our website' => 'Добро пожаловать на наш сайт',
 
     //share page
     'JoinNyata' => 'Присоединяйтесь к Ньята',
@@ -18,6 +18,7 @@ return [
     'GiveSupport' => 'Оказать поддержку',
     'SendYourDonation' => 'Отправить свое пожертвование на',
     'Join' => 'Присоединиться',
+    "LearnMore" => "Узнать больше",
 
     //menu
     'DonateMenu' => 'Donate',
@@ -28,4 +29,23 @@ return [
     'TravelCharityMenu' => 'Travel Charity',
     'ActOfKindnessMenu' => 'Act Of Kindness',
     'TakeActionMenu' => 'Take Action',
+
+    //ABOUT US PAGE
+    "FoundingHistory" => "Founding History",
+    "FoundingHistoryDescription" => "Started from the social service program held in high school every year, Nicole Oeij Teng, Pamela Berliana Sofian, and their friends and relatives were moved to continue their social service. Until 2013, Nicole decided to volunteer to teach in underprivileged schools and communites in Africa for 2 months. This experience gave birth an idea to help underprivileged and out of school children in Indonesia. This non-profit foundation was officially founded in 2014 by Nicole and Pamela and it keeps growing since then.",
+    "OurJourney" => "Our Journey",
+    "Home" => "Home",
+    "HomeDescription" => "Mountain, a historic symbol of Indonesia, represents home to the children we are taking care of.",
+    "Education" => "Education",
+    "EducationDescription" => "Education as the base of our value that we hold tightly to.",
+    "Children" => "Children",
+    "ChildrenDescription" => "Like a flower that is blooming, we focus on nurturing children of the nation.",
+    "Light" => "Light",
+    "LightDescription" => "Our children of light are our enlightment to the future",
+    "OurResults" => "Our Results",
+    "TagLine" => "Anak Nyata, Anak Terang",
+    "TagLineDescription" => "Anak terang dari negeri Indonesia.",
+
+    //TITLE
+    "AboutUsTitle" => "ABOUT US",
 ];
