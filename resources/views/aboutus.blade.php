a322d0afd6c176c181537c64dec82d49e4dfc547
Randy Kurnia <clodeo.randy@sicepat.com>
2023-07-07 16:13:32 +0700

    section our journey

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,6 +1,183 @@
 @extends('layout.default')
+@section('title', "ABOUT US")
 @section('page-content')
-    <section class="flex relative max-w-none w-full h-[400px] md:h-[950px]">
+
+{{-- header image --}}
+<section class="flex relative bg-cover bg-center bg-no-repeat bg-[url('/image/donate/HomePageAnakTerangNyata.jpg')]">
+    <div class="px-8 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-[#ff0000] md:text-5xl lg:text-6xl">ANAK TERANG</h1>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">ANAK NYATA</h1>
+    </div>
+</section>
+
+{{-- FOUNDING STORY --}}
+<section class="text-center max-w-none w-full bg-white py-16">
+    <h1 class="text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl px-24 uppercase">{{ __('messages.FoundingHistory') }}</h1>
+    <div class="inline-flex items-center justify-center w-full px-24">
+        <hr class="w-64 h-[2px] my-6 bg-gray-400 border-0 dark:bg-gray-700">
+    </div>
+    <p class="text-black dark:text-gray-400 px-8 py-6 tracking-wider">{{ __('messages.FoundingHistoryDescription') }}</p>
+</section>
+
+{{-- OUR JOURNEY --}}
+<section class="text-center max-w-none w-full bg-neutral-200 py-16">
+    <h1 class="text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl px-8 uppercase">{{ __('messages.OurJourney') }}</h1>
+    <div class="inline-flex items-center justify-center w-full px-24">
+        <hr class="w-64 h-[2px] my-6 bg-gray-400 border-0 dark:bg-gray-700">
+    </div>
+    <div class="flex flex-col md:grid grid-cols-9 mx-auto p-8">
+        <!-- left -->
+        <div class="flex flex-row-reverse md:contents">
+            <div class="bg-[#ff0000] col-start-1 col-end-5 p-8 rounded-[28px] my-4 ml-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2014</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Birth of the foundation
+                    </p>
+                </li>
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Kupang Program
+                    </p>
+                </li>
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Bandung & Jakarta Event
+                    </p>
+                </li>
+            </ul>
+            </div>
+            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div
+                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"
+            ></div>
+            </div>
+        </div>
+        <!-- right -->
+        <div class="flex md:contents">
+            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"></div>
+            </div>
+            <div class="bg-[#ff0000] col-start-6 col-end-10 p-8 rounded-[28px] my-4 mr-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2015</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Flores
+                    </p>
+                </li>
+            </ul>
+            </div>
+        </div>
+        <!-- left -->
+        <div class="flex flex-row-reverse md:contents">
+            <div class="bg-[#ff0000] col-start-1 col-end-5 p-8 rounded-[28px] my-4 ml-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2016</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Jogjakarta
+                    </p>
+                </li>
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Pulau Sumba
+                    </p>
+                </li>
+            </ul>
+            </div>
+            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div
+                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"
+            ></div>
+            </div>
+        </div>
+        <!-- right -->
+        <div class="flex md:contents">
+            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"></div>
+            </div>
+            <div class="bg-[#ff0000] col-start-6 col-end-10 p-8 rounded-[28px] my-4 mr-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2017</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Malang
+                    </p>
+                </li>
+            </ul>
+            </div>
+        </div>
+        <!-- left -->
+        <div class="flex flex-row-reverse md:contents">
+            <div class="bg-[#ff0000] col-start-1 col-end-5 p-8 rounded-[28px] my-4 ml-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2014</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Birth of the foundation
+                    </p>
+                </li>
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Travel Charity Kupang Program
+                    </p>
+                </li>
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Bandung & Jakarta Event
+                    </p>
+                </li>
+            </ul>
+            </div>
+            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div
+                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"
+            ></div>
+            </div>
+        </div>
+        <!-- right -->
+        <div class="flex md:contents">
+            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+            <div class="h-full w-6 flex items-center justify-center">
+                <div class="h-full w-1 bg-[#666] pointer-events-none"></div>
+            </div>
+            <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#ff0000] shadow"></div>
+            </div>
+            <div class="bg-[#ff0000] col-start-6 col-end-10 p-8 rounded-[28px] my-4 mr-auto shadow-md h-fit w-[36rem] min-h-[12rem] md:w-[20rem] lg:w-[26rem] xl:w-[32rem]">
+            <h3 class="flex items-center py-2 mb-1 text-3xl font-bold text-white">2021</h3>
+            <ul class="list-disc text-left p-4">
+                <li>
+                    <p class="text-base font-normal text-white py-1 ">
+                        Adik Asuh 1000 children on several locations such as Flores, Kupang, Alor, Nias, Kalimanta, Papua
+                    </p>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+    {{-- <p class="text-black dark:text-gray-400 px-8 py-6">{{ __('messages.FoundingHistoryDescription') }}</p> --}}
+</section>
+
+
+
+    {{-- <section class="flex relative max-w-none w-full h-[400px] md:h-[950px]">
         <div style="background-image:url('/image/donate/HomePageAnakTerangNyata.jpg')"
             class="relative w-full max-w-none bg-cover bg-center bg-fixed">
             <div
@@ -37,7 +214,7 @@
                 </h3>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -right-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -63,7 +240,7 @@
                 </div>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -left-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -79,7 +256,7 @@
                 </div>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -right-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -100,7 +277,7 @@
                 </div>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -left-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -116,7 +293,7 @@
                 </div>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[500px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -right-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -132,7 +309,7 @@
                 </div>
             </li>
             <li class="relative w-full h-[200px] mb-10 ml-0">
-                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-xl px-10">
+                <div class="bg-[#ff0000] w-[350px] h-[200px] absolute left-[950px] rounded-[28px] px-10">
                     <span class="absolute flex items-center justify-center w-8 h-8 -left-16">
                         <img src="/image/other/RedDot.png" class="">
                     </span>
@@ -242,5 +419,5 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 @endsection
