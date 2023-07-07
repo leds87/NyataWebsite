0b7f34be49587f6f8d9c1ba6e60393013c3ee606
Randy Kurnia <clodeo.randy@sicepat.com>
2023-07-07 23:36:19 +0700

    add join form footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,23 @@
+{{-- FOUNDING STORY --}}
+<section class="text-center max-w-none w-full bg-neutral-200 py-16">
+    <h1 class="text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl px-24 uppercase">{{ __('messages.JoinNyata') }}</h1>
+    <p class="text-black dark:text-gray-400 px-8 py-6 max-w-[68rem] mx-auto tracking-wider">{{ __('messages.JoinNyataDescription') }}</p>
+    <form class="flex flex-col w-11/12 mx-auto text-black justify-center">
+        <div class="flex flex-wrap mx-auto w-full h-1/4 py-3 justify-center md:flex-row">
+            <input
+                class="w-11/12 mx-1.5 rounded-lg py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline max-w-xs"
+                id="name" type="name" placeholder="{{ __('messages.Name') }}">
+            <input
+                class="w-11/12 mx-1.5 rounded-lg py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline max-w-xs"
+                id="emailaddress" type="emailaddress" placeholder="{{ __('messages.EmailAddress') }}">
+        </div>
+        <button
+            class="mx-auto w-11/12 bg-[#ff0000] text-white text-base font-bold mt-8 py-3 px-0 rounded-2xl focus:outline-none focus:shadow-outline max-w-xs md:mt-2"
+            type="button">
+            {{ __('messages.Join') }}
+        </button>
+    </form>
+</section>
 <div class="mx-auto w-full max-w-screen-full">
     <div class="grid grid-cols-1 gap-8 px-4 py-6 lg:py-8 md:grid-cols-2">
         <div class="w-full text-center mx-auto md:text-left">
