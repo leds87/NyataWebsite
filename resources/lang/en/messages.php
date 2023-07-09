69b0606cbdb65532613bf5a116dbc77db7ebbae5
Randy Kurnia <clodeo.randy@sicepat.com>
2023-07-09 22:09:21 +0700

    page what we do

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -7,12 +7,12 @@ return [
     'Test' => 'Test is successful',
     'Join Nyata Foundation and help to change the world to become a better place' => 'Join Nyata Foundation and help to change the world to become a better place',
     'Welcome to our website' => 'Welcome to our website',
-    'THE ISSUE' => 'THE ISSUE',
-    'TheIssueDescription' => 'The Ministry of Education and Culture noted that there were 157,000 elementary to high school
-    students dropping out of school in the 2019/2020 school year. Most students who drop out of school
-    are at the elementary school level as many as 59,400 students. This is where NYATA moves to help the
-    goverment and Indonesian children to reduce the high dropout rate and hopes that Indonesian children
-    will continue to get a proper education through several programs that NYATA has',
+    // 'THE ISSUE' => 'THE ISSUE',
+    // 'TheIssueDescription' => 'The Ministry of Education and Culture noted that there were 157,000 elementary to high school
+    // students dropping out of school in the 2019/2020 school year. Most students who drop out of school
+    // are at the elementary school level as many as 59,400 students. This is where NYATA moves to help the
+    // goverment and Indonesian children to reduce the high dropout rate and hopes that Indonesian children
+    // will continue to get a proper education through several programs that NYATA has',
 
     //share page
     'JoinNyata' => 'Join Nyata',
@@ -59,4 +59,16 @@ return [
 
     //What we do page
     'WhatWeDoTitle' => 'WHAT WE DO',
+    'TheIssue' => 'The Issue',
+    'TheIssueDescription' => 'The Ministry of Education and Culture noted that there were 157,000 elementary to high school students dropping out of school in the 2019/2020 school year. Most students who drop out of school are at the elementary school level as many as 59,400 students. This is where NYATA moves to help the goverment and Indonesian children to reduce the high dropout rate and hopes that Indonesian children will continue to get a proper education through several programs that NYATA has',
+    'WhatWeHold' => 'What We Hold',
+    'DirectGiving' => 'Direct Giving',
+    'DirectGivingDescription' => 'Donations given are directly channled to NYATA partner schools / other ongoing programs without going through intermediaries',
+    'LocalSupport' => 'Local Support',
+    'LocalSupportDescription' => 'Participate in supporting the welfare of local communities in obtaining a decent wage and life',
+    'OperationalIntegrity' => 'Operational Integrity',
+    'OperationalIntegrityDescription' => 'NYATA is proactive in ensuring that every ongoing activity and program can be carried out sustainably',
+    'OurSolution' => 'Our Solution',
+    'LearnMore' => 'Learn More'
+
 ];
