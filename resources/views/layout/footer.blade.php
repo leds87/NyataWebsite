336ec180e07873ff55c32f44b3ee2e521e206a2e
leds87 <edwinedun@gmail.com>
2023-08-10 14:48:44 +0700

    Revert "fix heading footer"
    This reverts commit baa07287a17d2dbf387bfabd41bbd529f06b3ec1.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,9 +1,8 @@
 {{-- JOIN FORM --}}
 <section class="text-center max-w-none w-full bg-neutral-200 py-16">
-    <h1 class="title-body text-black uppercase">{{ __('messages.JoinNyata') }}</h1>
-    {{-- <p class="text-black dark:text-gray-400 px-8 py-6 max-w-[68rem] mx-auto tracking-wider">{{ __('messages.JoinNyataDescription') }}</p> --}}
-    <p class="content-body px-10 py-5 text-center text-black max-w-lg mx-auto xl:max-w-none">{{ __('messages.JoinNyataDescription') }}</p>
-    <form class="flex flex-col w-11/12 mx-auto justify-center">
+    <h1 class="text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl px-24 uppercase">{{ __('messages.JoinNyata') }}</h1>
+    <p class="text-black dark:text-gray-400 px-8 py-6 max-w-[68rem] mx-auto tracking-wider">{{ __('messages.JoinNyataDescription') }}</p>
+    <form class="flex flex-col w-11/12 mx-auto text-black justify-center">
         <div class="flex flex-wrap mx-auto w-full h-1/4 py-3 justify-center md:flex-row">
             <input
                 class="w-11/12 mx-1.5 rounded-lg py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline max-w-xs"
@@ -12,7 +11,9 @@
                 class="w-11/12 mx-1.5 rounded-lg py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline max-w-xs"
                 id="emailaddress" type="emailaddress" placeholder="{{ __('messages.EmailAddress') }}">
         </div>
-        <button type="button" class="px-3 py-2 bg-red-600 text-xl rounded-[10px] uppercase w-[200px] mx-auto">
+        <button
+            class="mx-auto w-11/12 bg-[#ff0000] text-white text-base font-bold mt-8 py-3 px-0 rounded-2xl focus:outline-none focus:shadow-outline max-w-xs md:mt-2"
+            type="button">
             {{ __('messages.Join') }}
         </button>
     </form>
@@ -28,7 +29,7 @@
             <p class="text-xl"><a href="https://www.instagram.com/nyatafoundation/" target="blank">IG @nyatafoundation</a></p>
         </div>
         <div class="text-center md:text-right md:pt-16">
-            <button class="mx-auto w-[260px] bg-white text-[#ff0000] text-base font-bold py-3 rounded-2xl focus:outline-none focus:shadow-outline uppercase max-w-xs"
+            <button class="mx-auto w-full bg-white text-[#ff0000] text-base font-bold py-3 rounded-2xl focus:outline-none focus:shadow-outline uppercase max-w-xs"
                 type="button" onclick="window.location='{{ route('contactus') }}'">
                 {{ __('messages.GiveSupport') }}
             </button>
